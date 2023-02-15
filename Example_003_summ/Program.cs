a38cc20662c9b1346e21fb3267f804b01989d902
«Alleria» <lesok.22.12.95@mail.ru>
2023-02-16 01:23:20 +0500

    Example_003_random

--- Example_003_summ/Program.cs
+++ Example_003_summ/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 3;
-int nuberB = 5;
-Console.WriteLine (numberA + nuberB);
+﻿int numberA = new Random ().Next(1,10);
+Console.WriteLine (numberA);
+int numberB = new Random ().Next(1,10);
+Console.WriteLine (numberB);
+Console.WriteLine (numberA + numberB);
