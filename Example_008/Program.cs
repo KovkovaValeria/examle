822dc53fc445a9acda1dd95436a38824da337980
«Alleria» <lesok.22.12.95@mail.ru>
2023-02-16 23:43:40 +0500

    array

---
+++ Example_008/Program.cs
@@ -0,0 +1,20 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+	int result = arg1;
+	if (arg2> result) result = arg2;
+	if (arg3> result) result = arg3;
+	return result;
+}
+int[] array = {1,2,3,34,5,9,77,8,9};
+int n = array.Length;
+int find = 9;
+int index = 0;
+while (index<n)
+{
+if (array[index] == find)
+{
+	Console.WriteLine(index);
+	break;
+}
+	index++;
+}
